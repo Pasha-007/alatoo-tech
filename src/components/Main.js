8abfbf9b083d317f0f1542e626b16872236bf4be
Muntahaa Khan <103258353+Pasha-007@users.noreply.github.com>
2024-06-12 19:05:54 +0600

    Form added

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,6 +7,7 @@ import profileImage from '../assets/content_1.jpeg';
 import WhyATS from './whyATS';
 import Community from './Community';
 import Footer from './Footer';
+import MyForm from './MyForm';
 
 
 const Main = () => {
@@ -15,7 +16,7 @@ const Main = () => {
       {/* <Header showProfileButton={true} /> */}
       <section className="content">
         <div className="text">
-          <h1>Quality and Experience</h1>
+          <h1>Quality and Experience</h1> 
           <p>
             Quality and Experience: Elevate Your Learning Journey with Us! Our courses embody the pinnacle of educational excellence, carefully crafted to meet the highest standards. With years of teaching expertise, we ensure an enriching experience that equips you with the skills needed to succeed. Join us and experience the difference of learning with a team committed to both quality and experience.
           </p>
@@ -27,7 +28,7 @@ const Main = () => {
       <Courses />
       <WhyATS />
       <Community />
-      
+      <MyForm />
       <Footer />
     </div>
   );
